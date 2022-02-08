97c71d283cb988a1edae1cd886de70b0724859dd
robertjamison <65142411+robertjamison@users.noreply.github.com>
2022-02-08 16:30:20 -0500

    Update setup.py
    Restricted Python3 Versions in classifiers

--- setup.py
+++ setup.py
@@ -24,8 +24,14 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 )
